efe2bcc4f5e69fe70b50b9a2d705d62393c11f00
huanghaoyang <xiaotimel@163.com>
2026-02-09 11:58:58 +0800

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,24 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username = project.findProperty("sonatypeUsername")?.toString() ?: ""
+            password = project.findProperty("sonatypePassword")?.toString() ?: ""
+        }
+    }
+
+    // 发布所有子项目的 publications
+    // 使用这个可以简化配置
+    packageGroup.set("com.xiaotimel.im")
+}
+
