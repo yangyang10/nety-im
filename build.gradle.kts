a62312d92dac72fe42a1da17e4b65b1f69304962
huanghaoyang <xiaotimel@163.com>
2026-02-10 16:35:08 +0800

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,16 @@ plugins {
 nexusPublishing {
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username = project.findProperty("sonatypeUsername")?.toString() ?: ""
-            password = project.findProperty("sonatypePassword")?.toString() ?: ""
+            // 发布到 Central Publishing Portal 的 staging endpoint
+            nexusUrl.set(uri("https://central.sonatype.com/api/v1/maven/"))
+
+            // 使用 Central Publishing Portal token
+            username = project.findProperty("centralUsername")?.toString() ?: ""
+            password = project.findProperty("centralPassword")?.toString() ?: ""
         }
     }
 
-    // 发布所有子项目的 publications
-    // 使用这个可以简化配置
-    packageGroup.set("com.xiaotimel.im")
+    // 所有子项目的 groupId
+    packageGroup.set("io.github.yangyang10")
 }
 
